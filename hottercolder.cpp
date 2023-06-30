a633402b2b0b273c7a1c567433ffef9b986257ba
Brendon Bernardino <brendonrsb@hotmail.com>
2023-06-30 00:01:46 -0300

    refatored

--- hottercolder.cpp
+++ hottercolder.cpp
@@ -1,122 +1,91 @@
 #include <iostream>
 using namespace std;
 
+#define JILL_NUMBER 453
+#define SIZE_LIST   1000
+
 #define FINISH_GAME 99
 #define SAME        0
 #define HOTTER      1
 #define COLDER      -1
 
-int jill_number; // The global variable to store Jill's number
-int previous_guess; // The global variable to store Jack's previous guess
+int previous_guess;
+int previous_diff = abs(previous_guess - JILL_NUMBER);
+int step_counter = 0;
 bool first_guess = true;
-int previous_diff = abs(previous_guess - jill_number);
-
-int passos = 0;
 
 int Guess(int G) {
-    int current_diff = abs(G - jill_number);
-
-    if (first_guess) {
+    int current_diff = abs(G - JILL_NUMBER);
+    
+    if (first_guess) { // Primeiro palpite sempre retorna SAME
         first_guess = false;
-        // previous_guess = G;
         previous_diff = current_diff;
-        return SAME; // Same for the first guess
+        return SAME;
     }
-  
-    if(G == jill_number) {
+    if(G == JILL_NUMBER) // Palpite certo, acaba o game
         return FINISH_GAME;
-    }
-    else if (current_diff == previous_diff) {
+    else if (current_diff == previous_diff) { // Nem mais longe nem mais perto
         previous_diff = current_diff;
         return SAME;
     } 
-    else if (current_diff < previous_diff) {
+    else if (current_diff < previous_diff) { // Mais próximo do que antes
         previous_diff = current_diff;
         return HOTTER;
     } 
-    else if (current_diff > previous_diff) {
+    else if (current_diff > previous_diff) { // Mais longe do que antes
         previous_diff = current_diff;
         return COLDER;
     }
+    return FINISH_GAME;
 }
 
 int HC(int N) {
     int low = 1;
     int high = N;
+    int guess = high; // Primeiro palpite é N
     previous_guess = 0;
-    int guess = high;
-    int contador = 17;
 
-    while (low < high) {//(contador > 0) {//(low < high)
+    while (low < high) {
+        cout << "Guess: " << guess << endl;
+        step_counter++;
 
-        // cout << "Guess: " << guess << endl;
+        int response = Guess(guess);
+        if (response == HOTTER) {
+            cout << "HOTTER" << endl;
+            if(previous_guess > guess) 
+                high = (guess + high)/2;
 
-        passos++;
-        // if (previous_guess != 0) {
-            int response = Guess(guess);
+            if(previous_guess < guess)
+                low = (guess + low)/2;
+        } 
+        else if (response == COLDER) {
+            cout << "COLDER" << endl;
+            if(previous_guess > guess)
+                low = (guess + low)/2;
 
-            // if(guess == previous_guess) {
+            if(previous_guess < guess)
+                high = (guess + high)/2;
+        }
+        else if (response == SAME)
+            cout << "SAME" << endl;
 
-            // }
+        else if(response == FINISH_GAME)
+            return guess;
 
-            if (response == HOTTER) {
-                // cout << "HOTTER" << endl;
-                if(previous_guess > guess) {
-                    high = (guess + high)/2;
-                }
-                if(previous_guess < guess) {
-                    low = (guess + low)/2;
-                }
-            } 
-            else if (response == COLDER) {
-                // cout << "COLDER" << endl;
-                if(previous_guess > guess) {
-                    low = (guess + low)/2;
-                }
-                if(previous_guess < guess) {
-                    high = (guess + high)/2;
-                }
-            }
-            else if (response == SAME) {
-                // cout << "SAME" << endl;
-            }
-            else if(response == FINISH_GAME) {
-                return guess;
-            }
-            previous_guess = guess;
+        previous_guess = guess;
+        cout << "[" << low << "," << high << ']' << endl;
+        if(response == SAME && guess == previous_guess) //Casos especiais
+            guess = guess + 1;
 
-            // cout << "[" << low << "," << high << ']' << endl;
-            if(response == SAME && guess == previous_guess) {
-                guess = guess + 1;
-            }
-            if(response != SAME) {
-                guess = (low + high) / 2;
-            }
-            // guess = (low + high) / 2; //(middle = guess)
-            // if(guess == previous_guess) {
-            //     guess = high;
-            // }
-        // }
-
-        // previous_guess = guess;
-        contador--;
+        if(response != SAME) //Padrão
+            guess = (low + high) / 2;
     }
-
     return low;
 }
 
 int main() {
-    // Example usage
-    // for(int i=1; i<=100; i++) {
-    //     jill_number = i;//20;
-    //     int result = HC(100);
-    //     cout << "Jill's number is: " << result << endl;
-    //     cout << passos << " Passos.\n" << endl;
-    //     passos = 0;
-    // }
-    jill_number = 680;//20;
-    int result = HC(1000);
-    cout << "Jill's number is: " << result << endl;
-    cout << passos << " Passos." << endl;
+    int result = HC(SIZE_LIST);
+    cout << "O Numero de Jill eh: " << result << endl;
+    cout << step_counter << " passos." << endl;
     return 0;
 }
